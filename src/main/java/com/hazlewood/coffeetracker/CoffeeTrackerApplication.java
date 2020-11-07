6761699f9338b794f79329a1d9216a8d4c7f35a7
Joshua Hazlewood <joshhaz2012@gmail.com>
2020-11-07 19:02:30 +0000

    Initial commit - project generated with Spring Initilizr

---
+++ src/main/java/com/hazlewood/coffeetracker/CoffeeTrackerApplication.java
@@ -0,0 +1,53 @@
+package com.hazlewood.coffeetracker;
+
+import com.hazlewood.coffeetracker.beans.Beans;
+import com.hazlewood.coffeetracker.beans.BeansRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootApplication
+public class CoffeeTrackerApplication {
+  private static final Logger log = LoggerFactory.getLogger(CoffeeTrackerApplication.class);
+
+  public static void main(String[] args) {
+    SpringApplication.run(CoffeeTrackerApplication.class, args);
+  }
+
+  @Bean
+	@Profile("!test")
+  public CommandLineRunner demo(BeansRepository repository) {
+    return (args) -> {
+      // save some beans
+      repository.save(new Beans("India", 250));
+      repository.save(new Beans("Ethiopia", 500));
+      repository.save(new Beans("Guatemala", 250));
+      repository.save(new Beans("Kenya", 1000));
+
+      // fetch all beans
+      log.info("Beans found with findAll()");
+      log.info("-----------------------------");
+      for (Beans bean : repository.findAll()) {
+        log.info(bean.toString());
+      }
+      log.info("");
+
+      // fetch by id
+      Beans beans = repository.findById(1L).get();
+      log.info("Beans found with findById(1L)");
+      log.info("-----------------------------");
+      log.info(beans.toString());
+      log.info("");
+
+      // fetch bean by name
+      log.info("Beans found with findByName(Kenya)");
+      log.info("-----------------------------");
+      repository.findByName("Kenya").ifPresent(bean -> log.info(bean.toString()));
+      log.info("");
+    };
+  }
+}
---
+++ src/test/java/com/hazlewood/coffeetracker/CoffeeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hazlewood.coffeetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
