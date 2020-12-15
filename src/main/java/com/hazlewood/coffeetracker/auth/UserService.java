1a219d53b1b4e25d2ed87aa1d2fcbf8b187b9e3b
Joshua Hazlewood <joshhaz2012@gmail.com>
2020-12-15 16:20:48 +0000

    Overridden findByUsername in UserService

--- src/main/java/com/hazlewood/coffeetracker/auth/UserService.java
+++ src/main/java/com/hazlewood/coffeetracker/auth/UserService.java
@@ -1,7 +1,12 @@
 package com.hazlewood.coffeetracker.auth;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public interface UserService extends UserDetailsService {
   void save(User user);
+
+  @Override
+  UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
 }
